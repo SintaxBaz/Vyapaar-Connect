440dbcc46e2b2cf0db3f63792cc7bb902efb95f9
petercherian9 <petercherian9@gmail.com>
2026-01-16 01:48:44 +0530

    setup apk file & it worked on phone

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,11 +35,14 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = false
+            isShrinkResources = false
+
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+
 }
 
 flutter {
